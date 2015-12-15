d5f54d6b493c6986291ddd191bc0cafb1cd80cb8
Pete Cook <pete@cookpete.com>
2015-12-15 12:03:09 +0000

    Move progress logic from players to top level component
    * Now we're rendering all players at once, we don't want multiple progress loops going at once
    * Renamed `update` to `progress` – makes a bit more sense

--- src/players/Base.js
+++ src/players/Base.js
@@ -2,13 +2,10 @@ import { Component } from 'react'
 
 import { propTypes, defaultProps } from '../props'
 
-const UPDATE_FREQUENCY = 500
-
 export default class Base extends Component {
   static propTypes = propTypes
   static defaultProps = defaultProps
   componentDidMount () {
-    this.update()
     if (!this.props.url && this.prime) {
       this.priming = true
       this.prime()
@@ -16,7 +13,6 @@ export default class Base extends Component {
   }
   componentWillUnmount () {
     this.stop()
-    clearTimeout(this.updateTimeout)
   }
   componentWillReceiveProps (nextProps) {
     // Invoke player methods based on incoming props
@@ -38,21 +34,6 @@ export default class Base extends Component {
   }
   shouldComponentUpdate (nextProps) {
     return this.props.url !== nextProps.url
-  }
-  update = () => {
-    let progress = {}
-    const loaded = this.getFractionLoaded()
-    const played = this.getFractionPlayed()
-    if (!this.prevLoaded || loaded !== this.prevLoaded) {
-      progress.loaded = this.prevLoaded = loaded
-    }
-    if (!this.prevPlayed || played !== this.prevPlayed) {
-      progress.played = this.prevPlayed = played
-    }
-    if (progress.loaded || progress.played) {
-      this.props.onProgress(progress)
-    }
-    this.updateTimeout = setTimeout(this.update, UPDATE_FREQUENCY)
   }
   onReady = () => {
     this.setVolume(this.props.volume)
